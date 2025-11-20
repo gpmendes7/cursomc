995f97b3d4d0cc6d762f3cae38e7e5e35c7184c4
gpmendes7 <andrewsseven@gmail.com>
2025-11-20 13:31:48 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/nelialves/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.nelialves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelialves.cursomc.domain.Categoria;
+import com.nelialves.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+	
+}
